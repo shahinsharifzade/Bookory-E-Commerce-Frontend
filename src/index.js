99b71c9f4b9b2d5a84a436a6599caede9194b71b
sahinshf <shahinrsha@code.edu.az>
2025-01-08 22:14:32 +0400

    fix: Stripe configuration removed

--- src/index.js
+++ src/index.js
@@ -13,27 +13,27 @@ import { ToastContainer } from "react-toastify";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
+// import { Elements } from "@stripe/react-stripe-js";
 
 const router = createBrowserRouter(ROUTES);
 const queryClient = new QueryClient();
 
-const stripePromise = loadStripe(
-  "pk_test_51Nn1IUDU36UGP8uCILpcQwRVDL8KWaqrULBx2AE1SWvZIhwJ3isdTWpxa29c2iXVgu2FsBR77BqVk7NOp13e5jdT0089oN5GnB",
-);
+// const stripePromise = loadStripe(
+//   "pk_test_51Nn1IUDU36UGP8uCILpcQwRVDL8KWaqrULBx2AE1SWvZIhwJ3isdTWpxa29c2iXVgu2FsBR77BqVk7NOp13e5jdT0089oN5GnB",
+// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <Elements stripe={stripePromise}>
-          <App>
-            <RouterProvider router={router}></RouterProvider>
-            <ToastContainer />
-          </App>
-        </Elements>
+        {/* <Elements stripe={stripePromise}> */}
+        <App>
+          <RouterProvider router={router}></RouterProvider>
+          <ToastContainer />
+        </App>
+        {/* </Elements> */}
       </Provider>
       {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
